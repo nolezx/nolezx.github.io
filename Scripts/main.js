6d9e3a8d32dc0a1d35f64f1e93c34400b58c46eb
nolezx <nolezx@github.com>
2023-08-08 15:12:26 +0300

    'all'

--- Scripts/main.js
+++ Scripts/main.js
@@ -7,20 +7,3 @@ myImage.onclick = function () {
   } else {
     myImage.setAttribute("src", "Images/xperia-back.jpg");
   }
-};
-var myButton = document.querySelector("button");
-var myHeading = document.querySelector("h1");
-function setUserName() {
-  var myName = prompt("Please enter your name.");
-  localStorage.setItem("name", myName);
-  myHeading.textContent = "Инструкция по перепрошивке на Sailfish X, " + myName;
-}
-if (!localStorage.getItem("name")) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem("name");
-  myHeading.textContent = "Инструкция по перепрошивке на Sailfish X, " + storedName;
-}
-myButton.onclick = function () {
-  setUserName();
-};
