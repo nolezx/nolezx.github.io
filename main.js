2b9201aed2292c561a19371d8cca3f9617c5a9e8
nolezx <119740550+nolezx@users.noreply.github.com>
2023-08-13 10:33:56 +0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var myImage = document.querySelector("img");
-
-myImage.onclick = function () {
-  var mySrc = myImage.getAttribute("src");
-  if (mySrc === "images/firefox-icon.png") {
-    myImage.setAttribute("src", "Images/xperia-back.jpg");
-  } else {
-    myImage.setAttribute("src", "Images/xperia-back.jpg");
-  }
